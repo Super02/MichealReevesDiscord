aa8aedeb372bee942232bbd8cf52b45b6584aaf9
Super02 <34713084+Super02@users.noreply.github.com>
2018-10-21 17:24:53 +0200

    Update DiscordBot.py

--- EmojiBot-py/DiscordBot.py
+++ EmojiBot-py/DiscordBot.py
@@ -8,39 +8,39 @@ import webcolors
 bot = commands.Bot(command_prefix='-')
 
 @bot.event
-async def on_ready():
-	await bot.change_presence(game=discord.Game(name="-help"))
-	print ("Bot ready")
+async def on_ready(): # On ready
+	await bot.change_presence(game=discord.Game(name="-help")) # Set playing game to -help
+	print ("Bot ready") # Print information to console
 	print ("I am running on " + bot.user.name)
 	print ("With the ID: " + bot.user.id)
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) # Simple ping command
 async def ping(ctx):
 	await bot.say(":ping_pong: pong!")
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) # User information command
 async def info(ctx, user: discord.Member):
-	embed = discord.Embed(title="{}'s info".format(user.name), description="This is what I could find...", color=0x00ff00)
+	embed = discord.Embed(title="{}'s info".format(user.name), description="This is what I could find...", color=0x00ff00) # Creating embed
 	embed.add_field(name="Name", value=user.name, inline=True)
 	embed.add_field(name="ID", value=user.id, inline=True)
 	embed.add_field(name="Status", value=user.status, inline=True)
 	embed.add_field(name="Highest role", value=user.top_role, inline=True)
 	embed.add_field(name="Joined", value=user.joined_at, inline=True)
 	embed.set_thumbnail(url=user.avatar_url)
-	await bot.say(embed=embed)
-@bot.command(pass_context=True)
+	await bot.say(embed=embed) # Sending embed
+@bot.command(pass_context=True) # Server information command
 async def serverinfo(ctx):
-	embed = discord.Embed(title="{}'s info".format(ctx.message.server.name), description="Here is what I could find...", color=0x00ff00)
+	embed = discord.Embed(title="{}'s info".format(ctx.message.server.name), description="Here is what I could find...", color=0x00ff00) # Creating embed
 	embed.set_author(name="Info")
 	embed.add_field(name="Name", value=ctx.message.server.name, inline=True)
 	embed.add_field(name="ID", value=ctx.message.server.id, inline=True)
 	embed.add_field(name="Emojis", value=len(ctx.message.server.emojis), inline=True)
 	embed.add_field(name="Members", value=len(ctx.message.server.members), inline=True)
 	embed.set_thumbnail(url=ctx.message.server.icon_url)
-	await bot.say(embed=embed)
+	await bot.say(embed=embed) # Sending embed
 
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) # Kick command
 async def kick(ctx, user: discord.Member):
-	if(ctx.message.author.id == "305246941992976386" or ctx.message.author.server_permissions.kick_members and user.id != "305246941992976386"):
+	if(ctx.message.author.id == "305246941992976386" or ctx.message.author.server_permissions.kick_members and user.id != "305246941992976386"): # Checking for permissions
 		try:
 			await bot.kick(user)
 			embed = discord.Embed(title="<@{}> has been kicked succesfuly".format(user.id), color=0x00ff00)
@@ -56,9 +56,9 @@ async def kick(ctx, user: discord.Member):
 			embed.add_field(name="Permission", value="denied", inline=True)
 			await bot.say(embed=embed)
 	
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) # Ban command
 async def ban(ctx, user: discord.Member):
-	if(ctx.message.author.id == "305246941992976386" or ctx.message.author.server_permissions.ban_members and user.id != "305246941992976386"):
+	if(ctx.message.author.id == "305246941992976386" or ctx.message.author.server_permissions.ban_members and user.id != "305246941992976386"): # Checking for permissions
 		try:
 			await bot.ban(user)
 			embed = discord.Embed(title="<@{}> has been banned succesfuly".format(user.id), color=0x00ff00)
@@ -73,9 +73,9 @@ async def ban(ctx, user: discord.Member):
 		embed = discord.Embed(title="Error!", color=0xff0000)
 		embed.add_field(name="Permission", value="denied", inline=True)
 		await bot.say(embed=embed)
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) # Unban command 
 async def unban(ctx, user: discord.Member):
-	if(ctx.message.author.id == "305246941992976386" or ctx.message.author.server_permissions.ban_members):
+	if(ctx.message.author.id == "305246941992976386" or ctx.message.author.server_permissions.ban_members): # Checking for permissions
 		try:
 			await bot.unban(ctx.message.server, user)
 			embed = discord.Embed(title="<@{}> has been unbanned succesfuly".format(user.id), color=0x00ff00)
@@ -89,9 +89,9 @@ async def unban(ctx, user: discord.Member):
 		embed = discord.Embed(title="Error!", color=0xff0000)
 		embed.add_field(name="Permission", value="denied", inline=True)
 		await bot.say(embed=embed)
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) # Say command 
 async def say(ctx, *, arg):
-	if(ctx.message.author.id == "305246941992976386" or ctx.message.author.server_permissions.administrator):
+	if(ctx.message.author.id == "305246941992976386" or ctx.message.author.server_permissions.administrator): # Checking for permissions
 		await bot.say(arg)
 	else:
 		await bot.say("Error! You do not have permission for this command.")
@@ -107,7 +107,7 @@ async def hexcolor(ctx, *, arg):
 		except Exception as e:
 			await bot.say("Unknown error")
 			print(e)
-@bot.command(pass_context=True, aliases=['purge', 'prune'])
+@bot.command(pass_context=True, aliases=['purge', 'prune']) # Clear/Prune command bulk deletes text messages
 async def clear(ctx, amount=None):
 	try:
 		if not amount is None:
@@ -115,13 +115,13 @@ async def clear(ctx, amount=None):
 	except ValueError:
 		msg = await bot.say("Please put in a valid number!")
 		msgs=[]
-		async for msg in bot.logs_from(ctx.message.channel,limit=2):
-			msgs.append(msg)
+		async for msg in bot.logs_from(ctx.message.channel,limit=2): 
+			msgs.append(msg) # Deleting command and error messages
 		await asyncio.sleep(3)
 		await bot.delete_messages(msgs)
 		return
 	
-	if(ctx.message.author.id == "305246941992976386" or ctx.message.author.server_permissions.manage_messages):
+	if(ctx.message.author.id == "305246941992976386" or ctx.message.author.server_permissions.manage_messages): # Checking for permissions
 		msgs=[]
 		if amount is None or amount == 0 or amount >= 100:
 			async for msg in bot.logs_from(ctx.message.channel,limit=100):
@@ -133,7 +133,7 @@ async def clear(ctx, amount=None):
 		msg = await bot.say("Deleted {} messages.".format(len(msgs)-1))
 		await asyncio.sleep(5)
 		await bot.delete_message(msg)
-@bot.command(pass_context=True)
+@bot.command(pass_context=True) # Setgame command
 async def setgame(ctx, *, arg):
 	if(ctx.message.author.id == "305246941992976386"):
 		try:
@@ -143,4 +143,4 @@ async def setgame(ctx, *, arg):
 			bot.say("Unknown error")
 			print(type(e))
 
-bot.run("Sorry to say. You're not getting my Discord bot token xD")
+bot.run("Sorry to say. You're not getting my Discord bot token xD") # Running of my bot token
